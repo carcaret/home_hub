bd35b9d5754ad3c0d3114b92e0dc0d664e728837
carcaret <carcaret@icloud.com>
2017-09-18 14:30:11 +0200

    Calculate RMS of chunks and store in buffer

--- camera/audio/audio_recorder.py
+++ camera/audio/audio_recorder.py
@@ -1,13 +1,18 @@
 import pyaudio
-import wave
+import audioop
 
+from basic_buffer import Buffer
 from percentile_buffer import PBuffer
 
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 CHUNK = 16384
+SECONDS = 5
  
+def rms(audio_buffer):
+    return audioop.rms(bytes(audio_buffer.get()), 2)
+
 audio = pyaudio.PyAudio()
    
 stream = audio.open(format=FORMAT, channels=CHANNELS,
@@ -15,13 +20,15 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
                     rate=RATE, input=True,
                     frames_per_buffer=CHUNK)
 
-pbuffer = PBuffer(RATE * SECONDS)
+audio_buffer = Buffer(RATE * SECONDS)
+rms_buffer = PBuffer(int(RATE / CHUNK * SECONDS))
 
 while(True):
     for i in range(0, int(RATE / CHUNK)):
-        pbuffer.put(stream.read(CHUNK))
-    print(pbuffer.percentile(50))
+        audio_buffer.put(stream.read(CHUNK))
+    rms_buffer.put([rms(audio_buffer)])
                                   
 stream.stop_stream()
 stream.close()
 audio.terminate()
+
