f1cfb2b7ec7885c48029d47913f0b5ceca27d5a0
carcaret <carcaret@icloud.com>
2017-11-13 10:35:47 +0100

    Push values to client

--- camera/audio_socket.py
+++ camera/audio_socket.py
@@ -4,17 +4,31 @@ from channels import Channel
 
 from .audio.audio_recorder import Recorder
 
+MAX_VALUE=3000.0
+
 recorder = Recorder()
 thread_started = False
 
 class Sender:
+    def __init__(self, channel):
+        self.channel = channel
+
+    def normalize(self, value):
+        if value > MAX_VALUE:
+            value = MAX_VALUE
+        return value / MAX_VALUE
+
     def update(self, arg):
-        print("new value: {}".format(arg))
+        self.channel.send({
+            "text": json.dumps({
+                "message": self.normalize(arg),
+            })
+        })
 
 def ws_connect(message):
     if thread_started == False:
         thread_started == True
-        sender = Sender()
+        sender = Sender(message.reply_channel)
         recorder.register_observer(sender)
         _thread.start_new_thread(recorder.start, ())
 
